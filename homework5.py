f531254236a0d91a351496d09a146fd37fcb7c0e
Karelec <167227932+Karelec@users.noreply.github.com>
2024-04-22 21:36:24 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -2,6 +2,7 @@ my_list = ["Green", "Red", "Blue", "Orange", "Brown"]
 print(my_list)
 print('Первый элемент: ', my_list[0])
 print('Последний элемент: ', my_list[-1])
+print('Элементы с третьего по пятый: ', my_list[2:])
 my_list[2] = "Yellow"
 print(my_list)
 my_dict = {'RJ-45': 8, 'RJ-11': 4, 'COM': 9, 'VGA': 15}
